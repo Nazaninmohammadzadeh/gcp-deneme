93345ee86dbac23cbde5c47de6614b29466b2f0f
Nazanin Mohammadzadeh <nznnmhmmdzdh80@gmail.com>
2025-11-25 09:57:24 +0300

    Deleted Merhaba

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     # Cloud Run'ın otomatik olarak sağladığı PORT değişkenini okur
     # Eğer bu değişken yoksa, lokalde test için 8080 portunu kullanır
     port = int(os.environ.get('PORT', 8080))
-    return f"Merhaba CI/CD! Otomasyon çalışıyor! Port: {port} V4"
+    return f"CI/CD! Otomasyon çalışıyor! Port: {port} V4"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
